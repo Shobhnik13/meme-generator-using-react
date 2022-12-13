975ccc931ef8eadbae5f8b346d2758078cd60e59
shobhnik13 <shobhnikw@gmail.com>
2022-12-14 03:06:33 +0530

    added header

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <header className='header'>
+        <img src='../images/logo.svg' className='header-img'></img>
+        <h2 className='header-title' >Meme generator</h2>
+    </header>  )
+}
+
+export default header
