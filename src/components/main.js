301bf063f74bb9a9c167a93ae397fa345b4c422f
shobhnik13 <shobhnikw@gmail.com>
2022-12-14 04:28:48 +0530

    main added

---
+++ src/components/main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const main = () => {
+  return (
+    <div className='form'>
+         <input type="text" className='text-input' placeholder='Top text'/>
+        <input type="text" className='text-input' placeholder='Bottom text'/>
+        <button className='meme-button'>Get meme image</button>
+    </div>
+  )
+}
+
+export default main
