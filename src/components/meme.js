9422246e851ceff1269c2c307c6cb521b6a7386e
shobhnik13 <shobhnikw@gmail.com>
2022-12-20 01:46:52 +0530

    refractor state added

--- src/components/meme.js
+++ src/components/meme.js
@@ -2,11 +2,20 @@ import React from 'react'
 import memeData from '../memedata'
 function meme(){  
 
-  const[memeImg,setMemeImage]=React.useState("");
+  const [meme,setMeme]=React.useState({
+    topText:"", 
+    bottomText:"",
+    randomImage:""
+  })
+  const [allMemeImages,setAllMemeImages]=React.useState(memeData)
   function getMemeImage(){
     const memeArray=memeData.data.memes;
-    const randomPick=Math.floor(Math.random()+memeArray.length);
-    setMemeImage(memeArray[randomPick].url);
+    const randomPick=Math.floor(Math.random()*memeArray.length);
+    const url=memeArray[randomPick].url
+    setMeme(prevMeme=>({
+      ...prevMeme,
+      randomImage:url
+    }));
   }
   return (
     <main>
@@ -15,7 +24,7 @@ function meme(){
         <input type="text" className='text-input' placeholder='Bottom text'/>
         <button className='meme-button' onClick={getMemeImage}>Get meme image</button>
     </div>
-      <img src={memeImg} className='meme-image' ></img>
+      <img src={meme.randomImage} className='meme-image' ></img>
     </main>
   )
 }
