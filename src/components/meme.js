260f6430c7fb5c28cd28b2d20e0127d9e9af209b
shobhnik13 <shobhnikw@gmail.com>
2022-12-25 23:01:43 +0530

    added text to image

--- src/components/meme.js
+++ src/components/meme.js
@@ -17,14 +17,39 @@ function meme(){
       randomImage:url
     }));
   }
+  function handleChange(event){
+    const {name,value}=event.target
+    setMeme(prevMeme =>({
+      ...prevMeme,
+        [name]: value
+
+    }))
+  }
+
   return (
     <main>
     <div className='form'>
-         <input type="text" className='text-input' placeholder='Top text'/>
-        <input type="text" className='text-input' placeholder='Bottom text'/>
+         <input type="text"
+          className='text-input'
+           placeholder='Top text'
+           name='topText'//use the name as the same as mentioned in use state 
+           value={meme.topText}//call by using  state and the name used in state
+           onChange={handleChange}
+           />
+        <input type="text"
+         className='text-input'
+          placeholder='Bottom text'
+          name='bottomText'
+          value={meme.bottomText}
+          onChange={handleChange}
+          />
         <button className='meme-button' onClick={getMemeImage}>Get meme image</button>
     </div>
+    <div className="meme-corner">
       <img src={meme.randomImage} className='meme-image' ></img>
+      <h2 className="meme-toptext">{meme.topText}</h2>
+      <h2 className="meme-bottomtext">{meme.bottomText}</h2>
+    </div>
     </main>
   )
 }
